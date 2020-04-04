71f9761a2808fdf7e99926f3e2690c51fd65b5d5
José D. Polo Bolaño <jd45p8@gmail.com>
2020-04-03 20:09:25 -0500

    Se aceptan peticiones de todos los origenes.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const userRouter = require('./src/routes/user');
 const recordingRouter = require('./src/routes/recording');
 const phraseRouter = require('./src/routes/phrase');
@@ -9,6 +10,7 @@ const port = process.env.PORT || 8080;
 const dbDir = `${process.env.DB_PREFIX}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`;
 
 const app = express();
+app.use(cors());
 
 // Middleware para convertir en objetos la información de todas las solicitudes
 app.use(bodyParser.json());
